a787a98686f910bd4ee7fd71500da8c27d75dfe9
Sean Orosz <seanorosz@yahoo.com>
2022-11-28 23:24:29 -0500

    added dummy images

--- utils/bubbles.js
+++ utils/bubbles.js
@@ -2,16 +2,20 @@ window.onload = () => init();
 
 function init () {
     let bubbleElements = [];
-    let numElements = 15;
-    let minSize = 50;
-    let maxSize = 200;
+    let numElements = 9;
+    let minSize = 75;
+    let maxSize = 300;
     let bubbleWrapper = document.querySelector('.main');
+    let imgNodeList = document.querySelectorAll('.bubbleImg');
+    let imgArray = Array.from(imgNodeList);
+    if (imgArray.length < numElements) numElements = imgArray.length;
     for(let elemIndex = 0; elemIndex < numElements; elemIndex++) {
         let bubble = document.createElement('div');
-        let size = Math.floor((Math.random() * (maxSize - minSize) + 1));
+        let size = Math.floor((Math.random() * (maxSize - minSize) + minSize));
         bubble.classList.add('bubbleDiv');
-        bubble.style.width = `${size.toString()}px`;
         bubble.style.height = `${size.toString()}px`;
+        bubble.style.width = `${size.toString()}px`;
+        bubble.appendChild(imgArray[elemIndex]);
         bubbleElements.push(bubble);
     }
     bubbleElements.forEach(bub => {
