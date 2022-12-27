30d587341f05ce9880adc843e571bb16ee895dca
Sean Orosz <seanorosz@yahoo.com>
2022-12-27 17:07:20 -0500

    optimized data storage by placing all image info into an array of objects with img src and orientation properties

--- utils/bubbles.js
+++ utils/bubbles.js
@@ -7,37 +7,34 @@ const init = () => {
     let intervalTime = 0;
     let availableQuadrants = [0, 1, 2, 3];
     let bubbleWrapper = document.querySelector('.bubbleHolder');
-    let full = Array.from(document.querySelectorAll('.bubbleImg')).slice();
-    let imgArray = full.slice();
+    let tempImgArray = imgObjArray.slice();
     let numStartingBubbles = 1;
     let bubbles = 0;
-    const tryToCreateBubble = (startingBubbles, bubbles, full, imgArray, wrap, availableQuadrants) => {
+    const tryToCreateBubble = (startingBubbles, bubbles, tempImgArray, wrap, availableQuadrants) => {
         let quadIndex = Math.floor(Math.random() * availableQuadrants.length);
         let quadArr = availableQuadrants.splice(availableQuadrants.indexOf(quadIndex), 1);
         let quad = quadArr[0];
         if(quad >= 0){
-            let imgIndex = Math.floor(Math.random() * imgArray.length);
-            let imgTagArr = imgArray.splice(imgIndex, 1);
-            let imgSrc = imgTagArr[0].src;
-            let imgOrientation = imgTagArr[0].dataset.orientation;
+            let imgIndex = Math.floor(Math.random() * tempImgArray.length);
+            let imgObjArr = tempImgArray.splice(imgIndex, 1);
             bubbles++;
             if(bubbles >= startingBubbles) intervalTime = 2000;
-            if(imgArray.length === 0) imgArray = full.slice();
-            createBubble(imgSrc, imgOrientation, wrap, quad, availableQuadrants);
+            if(tempImgArray.length === 0) tempImgArray = imgObjArray.slice();
+            createBubble(imgObjArr[0], wrap, quad, availableQuadrants);
         }
-        setTimeout(tryToCreateBubble, intervalTime, numStartingBubbles, bubbles, full, imgArray, bubbleWrapper, availableQuadrants);
+        setTimeout(tryToCreateBubble, intervalTime, numStartingBubbles, bubbles, tempImgArray, bubbleWrapper, availableQuadrants);
     }
-    setTimeout(tryToCreateBubble, intervalTime, numStartingBubbles, bubbles, full, imgArray, bubbleWrapper, availableQuadrants);
+    setTimeout(tryToCreateBubble, intervalTime, numStartingBubbles, bubbles, tempImgArray, bubbleWrapper, availableQuadrants);
 }
-const createBubble = (imgSrc, orientation, wrapper, quad, quads) => {
+const createBubble = (imgObj, wrapper, quad, quads) => {
     let bubble = document.createElement('div');
     bubble.className += 'bubbleDiv';
     bubble.dataset.quad = `${quad}`;
     let classString = getBubbleLocation(quad);
     bubble.className += ` ${classString}`;
     let imgEl = document.createElement('img');
-    imgEl.src = imgSrc;
-    if(orientation === 'portrait') {
+    imgEl.src = imgObj.src;
+    if(imgObj.orientation === 'portrait') {
         imgEl.classList.add('portraitImg')
     } else {
         imgEl.classList.add('landscapeImg');
