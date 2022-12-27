8d958e47f71ff387822aa8e98fee2357d9fc570d
Sean Orosz <seanorosz@yahoo.com>
2022-12-26 23:00:37 -0500

    added touch screen solution for contact popup tab

--- utils/index.js
+++ utils/index.js
@@ -1,13 +1,8 @@
 const setup = () => {
     let tab = document.querySelector('.contact');
     tab.addEventListener('click', () => {
-        console.log(window.screen.width);
         if(tab.style.bottom === '0px') {
-            if(window.screen.width <= 550) {
-                tab.style.bottom = '-100px';
-            } else {
-                tab.style.bottom = '-60px';
-            }
+            tab.style.bottom = "";
         } else {
             tab.style.bottom = '0px';
         }
