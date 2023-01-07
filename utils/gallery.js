37928a0b19427fe08ea85e94153ff7e41cabe623
Sean Orosz <seanorosz@yahoo.com>
2023-01-07 14:25:56 -0500

    began implementing gallery page functionality

--- utils/gallery.js
+++ utils/gallery.js
@@ -3,5 +3,18 @@ window.onload = () => {
     setup();
 }
 const galleryInit = () => {
-
+    const imgHolder = document.querySelector('#img-holder');
+    imgObjArray.forEach(obj => {
+        let imgEl = document.createElement('img');
+        imgEl.src = obj.src;
+        if(obj.orientation === 'landscape') {
+            imgEl.classList.add('gallery-img-landscape');
+        } else {
+            imgEl.classList.add('gallery-img-portrait');
+        }
+        let divEl = document.createElement('div');
+        divEl.classList.add('gallery-img-div');
+        divEl.appendChild(imgEl);
+        imgHolder.appendChild(divEl);
+    });
 }
