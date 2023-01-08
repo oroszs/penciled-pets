9356929717f1e668be44e846f0c379ae9bfd2629
Sean Orosz <seanorosz@yahoo.com>
2023-01-08 02:03:48 -0500

    significantly enhanced gallery page styling

--- utils/gallery.js
+++ utils/gallery.js
@@ -5,15 +5,17 @@ window.onload = () => {
 const galleryInit = () => {
     const imgHolder = document.querySelector('#img-holder');
     imgObjArray.forEach(obj => {
+        let divEl = document.createElement('div');
+        divEl.classList.add('gallery-img-div');
         let imgEl = document.createElement('img');
         imgEl.src = obj.src;
         if(obj.orientation === 'landscape') {
             imgEl.classList.add('gallery-img-landscape');
+            divEl.classList.add('landscape-div');
         } else {
             imgEl.classList.add('gallery-img-portrait');
+            divEl.classList.add('portrait-div');
         }
-        let divEl = document.createElement('div');
-        divEl.classList.add('gallery-img-div');
         divEl.appendChild(imgEl);
         imgHolder.appendChild(divEl);
     });
