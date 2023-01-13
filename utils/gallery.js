d4e185fa83415de03f6c820b8afb297a45a5df1c
Sean Orosz <seanorosz@yahoo.com>
2023-01-13 13:42:16 -0500

    added modal functionality

--- utils/gallery.js
+++ utils/gallery.js
@@ -17,6 +17,26 @@ const galleryInit = () => {
         }
         let wrap = document.createElement('div');
         wrap.classList.add('div-wrap');
+        wrap.onclick = () => {
+            let wrapper = document.querySelector('.wrapper');
+            let modalBg = document.createElement('div');
+            modalBg.classList.add('modal-bg');
+            let modalHolder = document.createElement('div');
+            modalHolder.classList.add('modal-holder');
+            let modalImg = document.createElement('img');
+            modalImg.classList.add('modal-img');
+            modalImg.src = obj.src;
+            if(obj.orientation === 'portrait') {
+                modalImg.classList.add('modal-img-portrait');
+            } else {
+                modalImg.classList.add('modal-img-landscape');
+            }
+            modalHolder.appendChild(modalImg);
+            modalBg.appendChild(modalHolder);
+            wrapper.appendChild(modalBg);
+            modalBg.onclick = () => modalBg.remove();
+            modalHolder.onclick = () => modalBg.remove();
+        }
         wrap.appendChild(imgEl);
         divEl.appendChild(wrap);
         imgHolder.appendChild(divEl);
