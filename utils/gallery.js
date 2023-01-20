b6afa485dd270b53a2ecefb10bcd67bddfe64977
Sean Orosz <seanorosz@yahoo.com>
2023-01-20 14:24:44 -0500

    fixed styling on button icons

--- utils/gallery.js
+++ utils/gallery.js
@@ -59,7 +59,6 @@ const galleryControlsInit = () => {
     holders.forEach(holder => {
         holder.onclick = () => {
             const holderChildren = holder.children;
-            console.log(typeof holderChildren);
             for(child of holderChildren) {
                 if(child.classList.contains('hidden-button')) {
                     child.classList.remove('hidden-button');
