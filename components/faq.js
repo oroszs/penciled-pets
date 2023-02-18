6e76e7378bea6a1e31be4d01d5cfe07a4710c77d
Sean Orosz <seanorosz@yahoo.com>
2023-02-18 18:19:13 -0500

    began restructuring info page into faq page

---
+++ components/faq.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    faqInit();
+}
+const faqInit = () => {
+    const faqButtons = Array.from(document.querySelectorAll('.faq-button'));
+    faqButtons.forEach(but => {
+        but.onclick = (e) => e.currentTarget.parentNode.children[1].classList.toggle('faq-collapsed');
+    });
+}
