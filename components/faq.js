dc242951b21d775a6a8c85dd6baeb2bf95f499ca
Sean Orosz <seanorosz@yahoo.com>
2023-02-21 22:40:10 -0500

    updated expand animation so that it is instant, only works when animation is complete

--- components/faq.js
+++ components/faq.js
@@ -4,6 +4,30 @@ window.onload = () => {
 const faqInit = () => {
     const faqButtons = Array.from(document.querySelectorAll('.faq-button'));
     faqButtons.forEach(but => {
-        but.onclick = (e) => e.currentTarget.parentNode.children[1].classList.toggle('faq-expanded');
+        but.onclick = (e) => expandFaq(e.currentTarget.parentNode.children[1]);
+    });
+}
+const expandFaq = (el) => {
+    if(el.classList.contains('in-progress')) {
+        return;
+    } else {
+        el.classList.add('in-progress');
+    }
+    el.style.height = '';
+    el.style.transition = 'none';
+    const startHeight = window.getComputedStyle(el).height;
+    el.classList.toggle('faq-collapsed');
+    const newHeight = window.getComputedStyle(el).height;
+    el.style.height = startHeight;
+    window.requestAnimationFrame(() => {
+        el.style.transition = '';
+        window.requestAnimationFrame(() => {
+            el.style.height = newHeight;
+        });
+    });
+    el.addEventListener('transitionend', () => {
+        el.style.height = '';
+        el.removeEventListener('transitionend', this);
+        el.classList.remove('in-progress');
     });
 }
